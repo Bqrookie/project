1acd9b7064b56ff588b22df77a23d1279fe38ae8
Bqrookie <balgfi@live.cn>
2019-06-21 19:56:15 +0800

    update bug

--- toc小插件/toc.py
+++ toc小插件/toc.py
@@ -3,11 +3,7 @@ import re
 with open('a.md', 'rb') as f:
     content = f.readlines()
 
-# print(type(content))
-
 for x in content:
-    # print(x.decode('utf-8'))
-    # continue
     res = x.decode('utf-8')
     ret = re.findall('^#', res)
     if ret:
@@ -15,4 +11,3 @@ for x in content:
         tmp_str = res.split(' ')[1].replace('\r\n', '').lstrip().rstrip()
         result = ' ' * num * 2 + '* [' + tmp_str + '](#' + tmp_str + ')'
         print(result[2:])
-        # print(res[2:])
