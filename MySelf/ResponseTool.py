99c6fc07e56c819198e6cf86a0ac5e73ff1f2a3e
Bqrookie <balgfi@live.cn>
2019-07-01 14:25:50 +0800

    update

--- MySelf/ResponseTool.py
+++ MySelf/ResponseTool.py
@@ -51,12 +51,14 @@ class ResponseTool:
         return user_agent
 
     def get_headers(self):
-        headers = {'user-agent': self.get_user_agents(),
-                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-                     'Accept-Encoding': 'gzip, deflate, br',
-                     'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7',
-                     'Cache-Control': 'max-age=0',
-                     'Connection': 'keep-alive'}
+        headers = {
+                    'user-agent': self.get_user_agents(),
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7',
+                    'Cache-Control': 'max-age=0',
+                    'Connection': 'keep-alive',
+                   }
 
         return headers
 
